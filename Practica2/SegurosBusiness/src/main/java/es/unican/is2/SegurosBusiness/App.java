3840fb2ebb3c5ab79a6368f2494109fd28244623
Jorge <joraleoca@gmail.com>
2026-02-25 12:07:30 +0100

    feat: inicio proyecto SegurosBusiness

---
+++ Practica2/SegurosBusiness/src/main/java/es/unican/is2/SegurosBusiness/App.java
@@ -0,0 +1,13 @@
+package es.unican.is2.SegurosBusiness;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
