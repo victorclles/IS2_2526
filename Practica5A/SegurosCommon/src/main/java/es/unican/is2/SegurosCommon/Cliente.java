ffb2febd711db3ed94e430d7050f348b5a27815d
Jorge <joraleoca@gmail.com>
2026-04-22 11:59:33 +0200

    feat: add equals and hashcode to Cliente and Seguro

---
+++ Practica5A/SegurosCommon/src/main/java/es/unican/is2/SegurosCommon/Cliente.java
@@ -0,0 +1,116 @@
+package es.unican.is2.SegurosCommon;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Clase que representa un cliente de la empresa de seguros
+ * Un cliente se identifica por su dni
+ */
+public class Cliente {
+
+    private String dni;
+
+    private String nombre;  
+    
+    private boolean minusvalia;
+
+    private List<Seguro> seguros = new LinkedList<Seguro>();
+
+    /**
+     * Constructor de la clase Cliente.
+     */
+    public Cliente(String dni, String nombre, boolean minusvalia) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.minusvalia = minusvalia;
+    }
+    
+	/**
+     * Retorna los seguros del cliente 
+     */
+    public List<Seguro> getSeguros() {
+        return seguros;
+    }
+    
+    /**
+     * Asigna la lista de seguros
+     */
+    public void setSeguros(List<Seguro> seguros) {
+        this.seguros = seguros;
+    }
+
+    /**
+     * Retorna el nombre del cliente.   
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Asigna el nombre del cliente
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    /**
+     * Retorna el dni del cliente.
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * Asigna el dni del cliente
+     * @param dni
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    /**
+     * Indica si el cliente es minusvalido
+     */
+    public boolean getMinusvalia() {
+    	return minusvalia;
+    }
+
+    /**
+     * Asigna la minusvalia del cliente
+     * @param minusvalia
+     */
+     public void setMinusvalia(boolean minusvalia) {
+        this.minusvalia = minusvalia;
+    }
+
+    @Override
+    public int hashCode() {
+        if (dni != null) return dni.hashCode();
+        if (nombre != null) return nombre.hashCode();
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null &&
+            obj.getClass() == Cliente.class &&
+            ((Cliente) obj).dni.equals(this.dni);
+    }
+    
+    /**
+     * Calcula el total a pagar por el cliente por 
+     * todos los seguros a su nombre
+     */
+    public double totalSeguros() {
+    	double precio = seguros
+        		.stream()
+        		.map(Seguro::precio)
+        		.reduce(0.0, Double::sum);
+    	
+        if (getMinusvalia()) {
+        	precio *= (1 - Seguro.DESCUENTO_MINUSVALIA);
+        }
+        
+    	return precio;
+    }
+}
---
+++ Practica5A/SegurosCommon/src/main/java/es/unican/is2/SegurosCommon/IGestionSeguros.java
@@ -0,0 +1,49 @@
+package es.unican.is2.SegurosCommon;
+
+import java.util.zip.DataFormatException;
+
+
+
+/**
+ * Interfaz de negocio para gestionar los
+ * seguros de la empresa de seguros
+ */
+public interface IGestionSeguros {
+	
+	/**
+	 * Agrega un nuevo seguro al cliente cuyo dni se indica.
+	 * @param s Seguro que desea agregar
+	 * @param dni DNI del cliente
+	 * @return El seguro agregado
+	 * 		   null si no se agrega porque no se encuentra el cliente
+	 * @throws OperacionNoValida si el seguro ya existe
+	 * @throws DataAccessException si se produce un error 
+	 * en el acceso a la base de datos
+	 */
+	public Seguro nuevoSeguro(Seguro s, String dni) throws OperacionNoValida, DataAccessException;
+	
+	/**
+	 * Elimina el seguro cuya matricula se indica y 
+	 * que pertenece al cliente cuyo dni se indica
+	 * @param matricula Identificador del seguro a eliminar
+	 * @param dni DNI del propietario del seguro
+ 	 * @return El seguro eliminado
+ 	 *         null si el seguro o el cliente no existen
+ 	 * @throws OperacionNoValida si el seguro no pertenece al dni indicado
+	 * @throws DataAccessException si se produce un error 
+	 * en el acceso a la base de datos
+	 */
+	public Seguro bajaSeguro(String matricula, String dni) throws OperacionNoValida, DataAccessException;
+
+	/**
+	 * Agrega o modifica el conductor adicional al seguro cuya matricula se indica
+	 * @param matricula Identificador del seguro
+	 * @param conductor Nombre del conductor adicional a agregar
+ 	 * @return El seguro modificado
+ 	 *         null si el seguro no existe
+	 * @throws DataAccessException si se produce un error 
+	 * en el acceso a la base de datos
+	 */
+	public Seguro anhadeConductorAdicional(String matricula, String conductor) throws DataAccessException;
+
+}
